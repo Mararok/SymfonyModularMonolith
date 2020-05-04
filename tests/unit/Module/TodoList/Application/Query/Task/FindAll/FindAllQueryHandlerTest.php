39fec2faa49fa77a86c68a145123602b0d342a97
awasiak <a.wasiak@sare.pl>
2020-05-04 15:31:26 +0200

    add phpunit and first test

---
+++ tests/unit/Module/TodoList/Application/Query/Task/FindAll/FindAllQueryHandlerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Module\TodoList\Application\Query\Task\FindAll;
+
+use App\Module\TodoList\Application\Query\Task\FindAll\FindAllQuery;
+use App\Module\TodoList\Application\Query\Task\FindAll\FindAllQueryHandler;
+use App\Module\TodoList\Domain\TaskRepository;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+
+
+class FindAllQueryHandlerTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function test__invoke()
+    {
+        $repository = \Mockery::mock(TaskRepository::class);
+        $handler = new FindAllQueryHandler($repository);
+
+        $expectedEntities = new \ArrayIterator([]);
+        $repository->expects("findAll")->andReturn($expectedEntities);
+
+        $currentEntities = $handler(new FindAllQuery());
+
+        $this->assertSame($expectedEntities, $currentEntities);
+
+    }
+}
